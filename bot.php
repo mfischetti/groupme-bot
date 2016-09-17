b8886b2906b4d5836106e63af78a26e89aaa6808
Mike <mfischetti2192@gmail.com>
2016-09-17 00:01:54 -0400

    added message variables

--- bot.php
+++ bot.php
@@ -5,5 +5,13 @@ $postData = file_get_contents( "php://input" );
 
 $p = json_decode($postData, true);
 
+//values from groupme message
+$msgid = $p['id'];
+$msgText = strtolower($p['text']); 
+$usrName = strtolower($p['name']);
+
+echo 'Message id: ' . $msgid;
+echo 'Message Text: ' . $msgText;
+echo 'Message username : ' . $usrName;
 
 ?>
