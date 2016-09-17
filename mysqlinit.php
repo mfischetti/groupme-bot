e72d5a88aa10f068dc2e764125edc0440f2fe316
Mike <mfischetti2192@gmail.com>
2016-09-17 00:45:49 -0400

    added mysql init file

---
+++ mysqlinit.php
@@ -0,0 +1,31 @@
+<?php
+/*************************************************************************************
+* mysqilinit.php
+* Run once to setup mysql table
+* 
+*************************************************************************************/
+
+//cleardb url for heroku
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = ltrim($url["path"], '/');
+
+$table = "messages";
+try {
+    $db = new PDO('mysql:host=' .$server. ';dbname=' .$db. ';charset=utf8mb4', $username, $password);
+    error_log("db connect");
+    $db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );//Error Handling
+    $sql ="CREATE table $table(
+     ID INT( 11 )  PRIMARY KEY,
+     Name VARCHAR( 50 ) NOT NULL, 
+     Msg VARCHAR( 250 ) NOT NULL);" ;
+     $db->exec($sql);
+     print("Created $table Table.\n");
+
+} catch(PDOException $e) {
+    echo $e->getMessage();
+}
+?>
