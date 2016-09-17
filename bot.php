976c672bc09381f370f3747d9f624a19aad73572
Mike <mfischetti2192@gmail.com>
2016-09-17 00:08:24 -0400

    added bot response back to group

--- bot.php
+++ bot.php
@@ -1,5 +1,8 @@
 <?php
 
+//grabs the groupme bot id from the envrionment variables
+$BOT_ID = getenv('BOT_ID'); 
+
 // Get the message contents and decode the json
 $postData = file_get_contents( "php://input" );
 
@@ -13,5 +16,9 @@ $usrName = strtolower($p['name']);
 echo 'Message id: ' . $msgid;
 echo 'Message Text: ' . $msgText;
 echo 'Message username : ' . $usrName;
+
+//bot repeats messages back to group
+$baseUrl = "https://api.groupme.com/v3/bots/post";
+$res = \Httpful\Request::post( $this->baseUrl )->sendsJson( )->body( '{"text":"'.  $msgText .'","bot_id":"' . $BOT_ID . '"}' )->send( );
 
 ?>
